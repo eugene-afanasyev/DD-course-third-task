a170be39a9ec6edbdc9061f6a1a43b39c155ae21
eugene-afanasyev <whoaeugene@gmail.com>
2020-12-29 02:45:51 +0900

    changed stylesheet, added search field UI

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,7 +1,38 @@
 package main;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class Controller {
+    @FXML
     public TextField searchField;
+    public BorderPane header;
+
+    public void initialize() {
+        initialzieSearchField();
+    }
+
+    public void initialzieSearchField() {
+        searchField.setFocusTraversable(false);
+
+        searchField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                searchField.getStyleClass().clear();
+                if (t1) {
+                    searchField.getStyleClass().add("searchFieldFocused");
+                } else {
+                    searchField.getStyleClass().add("searchField");
+                }
+            }
+        });
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
