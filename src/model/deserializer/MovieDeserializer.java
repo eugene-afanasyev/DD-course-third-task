ce3a982a404254b7466cd0e6023a64bbb1b38b28
eugene-afanasyev <whoaeugene@gmail.com>
2020-12-29 21:22:54 +0900

    MovieDeserializer implemented

--- src/model/deserializer/MovieDeserializer.java
+++ src/model/deserializer/MovieDeserializer.java
@@ -4,6 +4,9 @@ import com.google.gson.*;
 import model.Movie;
 
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MovieDeserializer implements JsonDeserializer<Movie> {
 
@@ -13,6 +16,40 @@ public class MovieDeserializer implements JsonDeserializer<Movie> {
         JsonObject jsonObject = src.getAsJsonObject();
 
         Movie movie = new Movie();
+        movie.setFilmId(jsonObject.get("filmId").getAsInt());
+        movie.setNameRu(jsonObject.get("nameRu").getAsString());
+        movie.setNameEn(jsonObject.get("nameEn").getAsString());
+        movie.setYear(jsonObject.get("year").getAsInt());
+        movie.setDescription(jsonObject.get("description").getAsString());
+
+        JsonArray countries = jsonObject.get("countries").getAsJsonArray();
+        ArrayList<String> countriesList = new ArrayList<>();
+        for (int i = 0; i < countries.size(); i++) {
+            countriesList.add(countries.get(0).getAsString());
+        }
+        movie.setCountries(countriesList);
+
+        JsonArray genres = jsonObject.get("genres").getAsJsonArray();
+        ArrayList<String> genresList = new ArrayList<>();
+        for (int i = 0; i < genres.size(); i++) {
+            countriesList.add(genres.get(0).getAsString());
+        }
+        movie.setGenres(genresList);
+
+        movie.setRating(jsonObject.get("rating").getAsDouble());
+
+        movie.setRatingVoteCount(jsonObject.get("ratingVoteCount").getAsInt());
+        try {
+            movie.setPosterUrl(new URL(jsonObject.get("posterUrl").getAsString()));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            movie.setPosterUrlPreview(new URL(jsonObject.get("posterUrlPreview").getAsString()));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         return movie;
     }
