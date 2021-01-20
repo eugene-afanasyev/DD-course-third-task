3f97e25f8a1240f7dbb849b5f8f4a89252c1bb05
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-20 22:30:10 +0900

    refactoring work

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,8 +16,10 @@ import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import kinopoisk.KinopoiskAPI;
 import model.Movie;
@@ -36,131 +39,200 @@ public class Controller {
     @FXML
     public ScrollPane contentScrollPane;
     @FXML
-    public BorderPane contentBorderPane;
-    @FXML
-    public VBox leftColumn;
-    @FXML
-    public VBox centerColumn;
-    @FXML
-    public Label filmNameRu;
-    @FXML
-    public Label filmNameEn;
-    @FXML
-    public Label filmDescription;
-    @FXML
-    public VBox rightColumn;
-    @FXML
-    public Label ratingLabel;
-    @FXML
-    public Label ratingIMDBLabel;
-    @FXML
     private TextField searchField;
-    @FXML
-    private BorderPane header;
 
-    KinopoiskAPI api = new KinopoiskAPI();
+    public BorderPane contentBorderPane = new BorderPane();
+    public VBox leftColumn = new VBox();
+    public VBox centerColumn = new VBox();
+    public VBox rightColumn = new VBox();
 
     public void initialize() {
+        searchField.setFocusTraversable(false);
+        searchField.focusedProperty().addListener(new ChangeListener<>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                searchField.getStyleClass().clear();
+                if (t1) {
+                    searchField.getStyleClass().add("searchFieldFocused");
+                } else {
+                    searchField.getStyleClass().add("searchField");
+                }
+            }
+        });
+
+        contentBorderPane.prefWidth(1024);
+        showFilmPageById(301);
+    }
+
+    public void search(MouseEvent event) {
+        KinopoiskAPI api = new KinopoiskAPI();
+        ArrayList<Movie> films = api.getMoviesByKeyword(searchField.getText());
+
+        VBox vbox = new VBox();
+        for (Movie film : films) {
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER);
+            hBox.setSpacing(15);
+            hBox.setStyle("-fx-padding: 5");
+
+            ImageView imageView =
+                    new ImageView(new Image(film.getPosterUrlPreview().toExternalForm()));
+            imageView.setFitHeight(150);
+            imageView.setPreserveRatio(true);
+
+            Label label = new Label(film.getNameRu());
+            label.setStyle("-fx-font-family: 'Open Sans'; -fx-font-size: 30;");
+
+            hBox.getChildren().addAll(imageView, label);
+            vbox.getChildren().add(hBox);
+        }
+        contentScrollPane.setContent(vbox);
     }
 
     public void showFilmPageById(int id)  {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/Views/filmPage.fxml"));
-            Scene scene = new Scene(root, 1080, 640);
-            Stage stage = (Stage) layout.getScene().getWindow();
-            scene.getStylesheets().add(getClass().getResource("/Stylesheets/style.css").toExternalForm());
-            stage.setScene(scene);
-            stage.setResizable(false);
-        } catch (IOException e) {
-            e.printStackTrace();
+        KinopoiskAPI api = new KinopoiskAPI();
+        Movie movie = api.getMovieById(id);
+        Image poster;
+        if (movie.getPosterUrl() != null) {
+            poster = new Image(movie.getPosterUrl().toExternalForm());
+        } else {
+            poster = null;
         }
-//        Movie movie = api.getMovieById(id);
-//        Image poster;
-//        if (movie.getPosterUrl() != null) {
-//            poster = new Image(movie.getPosterUrl().toExternalForm());
-//        } else {
-//            poster = null;
-//        }
-//        assert poster != null;
-//
-//        BackgroundImage movieBg= new BackgroundImage(
-//                poster, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-//                new BackgroundPosition(Side.LEFT, 0, false, Side.TOP, 0, false),
-//                new BackgroundSize(contentWrapper.getPrefWidth(), contentWrapper.getPrefHeight(),
-//                        false, false, false, true));
-//
-//        contentBackground.setBackground(new Background(movieBg));
-////        contentBackground.setEffect(new GaussianBlur());
-//
-//        // setting on left column content
-//        BackgroundImage moviePreviewBg = new BackgroundImage(
-//                new Image(movie.getPosterUrlPreview().toExternalForm()),
-//                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-//                new BackgroundPosition(Side.LEFT, 0, false, Side.TOP, 0, false),
-//                new BackgroundSize(contentWrapper.getPrefWidth(), contentWrapper.getPrefHeight(),
-//                        false, false, false, true));
-//        leftColumn.setBackground(new Background(moviePreviewBg));
-//
-//
-//        // setting on center column content
-//        filmNameRu.setText(movie.getNameRu());
-//        filmNameEn.setText(movie.getNameEn() + "   +" + movie.getRatingAgeLimits());
-//        filmDescription.setText(movie.getDescription());
-//
-//        generateInfoField("Год производства", movie.getYear());
-//        String countries = new String("");
-//        for (String country : movie.getCountries()) {
-//            if (countries.equals(""))
-//                countries = country;
-//            else
-//                countries += ", " + country;
-//        }
-//        generateInfoField("Страна", countries);
-//
-//        String genres = new String("");
-//        for (String genre : movie.getGenres()) {
-//            if (genres.equals(""))
-//                genres = genre;
-//            else
-//                genres += ", " + genre;
-//        }
-//        generateInfoField("Жанры", genres);
-//        generateInfoField("Слоган", movie.getSlogan());
-//        generateInfoField("Премьера в Росcии", movie.getPremiereRu());
-//        generateInfoField("Премьера в мире", movie.getPremiereWorld());
-//        generateInfoField("Длительность", movie.getFilmLength());
-//
-//        // setting on film frames
-//        ScrollPane scrollPane = new ScrollPane();
-//        scrollPane.setMaxHeight(300);
-//        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-//
-//        HBox hbox = new HBox();
-//        hbox.setPrefWidth(centerColumn.getMinWidth());
-//        hbox.setSpacing(5);
-//        hbox.setStyle("-fx-background-color: #c5cffc");
-//        hbox.setPadding(new Insets(15, 5, 15, 5));
-//        scrollPane.setContent(hbox);
-//
-//        for (Image image : api.getFramesById(movie.getFilmId())) {
-//            ImageView img = new ImageView(image);
-//            img.setStyle("-fx-border-color: white; -fx-border-width: 3; -fx-border-radius: 3");
-//            img.setPreserveRatio(true);
-//            img.setFitHeight(300);
-//            hbox.getChildren().add(img);
-//        }
-//        centerColumn.getChildren().add(scrollPane);
-//
-//        // setting on right column content
-//        ArrayList<Staff> actors = api.getActorsByFilmId(movie.getFilmId());
-//        for (Staff actor : actors) {
-//            Label l = new Label(actor.getNameRu());
-//            l.setStyle("-fx-wrap-text: true; -fx-font-size: 22; -fx-text-fill: #c9c9c9");
-//            rightColumn.getChildren().add(l);
-//        }
-//
-//        contentBorderPane.setLeft(leftColumn);
-//        contentBorderPane.setCenter(centerColumn);
-//        contentScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        BackgroundImage movieBg= new BackgroundImage(
+                poster, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                new BackgroundPosition(Side.LEFT, 0, false, Side.TOP, 0, false),
+                new BackgroundSize(contentWrapper.getPrefWidth(), contentWrapper.getPrefHeight(),
+                        false, false, false, true));
+
+        contentBackground.setBackground(new Background(movieBg));
+//        contentBackground.setEffect(new GaussianBlur());
+
+        // setting on left column content
+        ImageView preview = new ImageView();
+        preview.setImage(new Image(movie.getPosterUrlPreview().toExternalForm()));
+        preview.setFitWidth(250);
+        preview.setPreserveRatio(true);
+        leftColumn.getChildren().add(preview);
+        leftColumn.setStyle("-fx-padding: 10");
+
+        // setting on center column content
+        Label filmNameRu = new Label(movie.getNameRu());
+        filmNameRu.setStyle("-fx-text-fill: #ffffff; -fx-font-size: 40; -fx-font-weight: bold");
+        filmNameRu.setWrapText(true);
+
+        Label filmNameEn = new Label(movie.getNameEn() + "   +" + movie.getRatingAgeLimits());
+        filmNameEn.setStyle("-fx-text-fill: #868686; -fx-font-size: 28;");
+        filmNameEn.setWrapText(true);
+
+        Label filmDescription = new Label(movie.getDescription());
+        filmDescription.setStyle("-fx-text-fill: #ffffff; -fx-font-size: 18");
+        filmDescription.setWrapText(true);
+
+        Label aboutFilmLabelTitle = new Label("О фильме");
+        aboutFilmLabelTitle.setStyle("-fx-text-fill: #ffffff; -fx-font-size: 30;");
+        filmDescription.setWrapText(true);
+
+        centerColumn.getChildren().addAll(filmNameRu, filmNameEn, filmDescription, aboutFilmLabelTitle);
+
+        generateInfoField("Год производства", movie.getYear());
+        String countries = new String("");
+        for (String country : movie.getCountries()) {
+            if (countries.equals(""))
+                countries = country;
+            else
+                countries += ", " + country;
+        }
+        generateInfoField("Страна", countries);
+
+        String genres = new String("");
+        for (String genre : movie.getGenres()) {
+            if (genres.equals(""))
+                genres = genre;
+            else
+                genres += ", " + genre;
+        }
+        generateInfoField("Жанры", genres);
+        generateInfoField("Слоган", movie.getSlogan());
+        generateInfoField("Премьера в Росcии", movie.getPremiereRu());
+        generateInfoField("Премьера в мире", movie.getPremiereWorld());
+        generateInfoField("Длительность", movie.getFilmLength());
+
+        // setting on film frames
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setMaxHeight(300);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        HBox hbox = new HBox();
+        hbox.setPrefWidth(centerColumn.getMinWidth());
+        hbox.setSpacing(5);
+        hbox.setStyle("-fx-background-color: #c5cffc");
+        hbox.setPadding(new Insets(15, 5, 15, 5));
+        scrollPane.setContent(hbox);
+
+        for (Image image : api.getFramesById(movie.getFilmId())) {
+            ImageView img = new ImageView(image);
+            img.setStyle("-fx-border-color: white; -fx-border-width: 3; -fx-border-radius: 3");
+            img.setPreserveRatio(true);
+            img.setFitHeight(300);
+            hbox.getChildren().add(img);
+        }
+
+        Label framesLibelTitle = new Label("Кадры: ");
+        framesLibelTitle.setStyle("-fx-text-fill: white; -fx-font-size: 28");
+
+        centerColumn.getChildren().add(framesLibelTitle);
+        centerColumn.getChildren().add(scrollPane);
+
+        centerColumn.setSpacing(10);
+        centerColumn.setStyle("-fx-padding: 10");
+        centerColumn.setPrefWidth(700);
+
+        Label actorsLabelTitle = new Label("В главных ролях: ");
+        actorsLabelTitle.setStyle("-fx-text-fill: white; -fx-font-size: 30");
+        rightColumn.getChildren().add(actorsLabelTitle);
+
+        // setting on right column content
+        ArrayList<Staff> actors = api.getActorsByFilmId(movie.getFilmId());
+        int counter = 0;
+        for (Staff actor : actors) {
+            if (counter++ == 10)
+                break;
+
+            Label l = new Label(actor.getNameRu());
+            l.setStyle("-fx-wrap-text: true; -fx-font-size: 22; -fx-text-fill: #ffffff");
+            l.hoverProperty().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                    if (l.isHover()) {
+                        l.setTextFill(Color.DARKORANGE);
+                    } else {
+                        l.setTextFill(Color.WHITE);
+                    }
+                }
+            });
+
+            rightColumn.getChildren().add(l);
+        }
+
+        contentBorderPane.setLeft(leftColumn);
+        contentBorderPane.setRight(rightColumn);
+        contentBorderPane.setCenter(centerColumn);
+        contentScrollPane.setContent(contentBorderPane);
+        contentScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+    }
+
+    private void generateInfoField(String propName, String propValue) {
+        BorderPane pane = new BorderPane();
+        Label propNameLabel = new Label(propName);
+        Label propValueLabel = new Label(propValue);
+
+        propNameLabel.getStyleClass().add("prop-name-label");
+        propValueLabel.getStyleClass().add("prop-value-label");
+
+        pane.setLeft(propNameLabel);
+        pane.setRight(propValueLabel);
+
+        centerColumn.getChildren().addAll(pane, new Separator());
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,31 +1,18 @@
 package main;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import kinopoisk.KinopoiskAPI;
-import model.Actor;
-import model.Movie;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.fluent.Request;
-
-import java.util.List;
 
 public class Main extends Application {
     KinopoiskAPI kinopoiskAPI = new KinopoiskAPI();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/Views/mainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/main/mainView.fxml"));
         Scene mainScene = new Scene(root, 1280, 766);
         mainScene.setFill(null);
         mainScene.getStylesheets().add(getClass().getResource("/Stylesheets/style.css").toExternalForm());
